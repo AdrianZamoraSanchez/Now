68b83e0b42919ab44d241ddb1adc9adc99bd6fc1
AdrianZamoraSanchez <adrian@Adrian.>
2025-07-04 19:06:23 +0200

    CMakeLists and build environment

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,23 @@
 Main file
 
 */
+
+
+#include <iostream>
+#include "antlr4-runtime.h"
+#include "NowLexer.h"
+#include "NowParser.h"
+
+using namespace antlr4;
+
+int main(){
+	ANTLRInputStream input("2ms\n");
+	NowLexer lexer(&input);
+	CommonTokenStream tokens(&lexer);
+
+	NowParser parser(&tokens);
+	tree::ParseTree *tree = parser.prog();
+
+	std::cout << tree->toStringTree(&parser) << std::endl;
+	return 0;
+}
