ffba1e3a398177ed5b555379372bd9a9d5f4ca0a
AdrianZamoraSanchez <adrian@Adrian.>
2025-07-05 13:46:21 +0200

    Added compile script 'build.sh' and test file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 /*
 Main file
-
 */
 
 
@@ -8,11 +7,32 @@ Main file
 #include "antlr4-runtime.h"
 #include "NowLexer.h"
 #include "NowParser.h"
+#include "ios"
 
 using namespace antlr4;
 
 int main(){
-	ANTLRInputStream input("2ms\n");
+	std::string line;
+	std::string fileText = "";
+
+	std::ifstream testFile ("../input.now");
+
+	if (testFile.is_open()) {
+	  while (getline (testFile,line)) {
+	    fileText += line + '\n';
+	  }
+
+	  testFile.close();
+	} 
+	else {
+		std::cout << "Unable to open file" << std::endl;
+		return 1;
+	} 
+	
+	std::cout << "Text being analyzed" << std::endl;
+	std::cout << fileText << std::endl;
+
+	ANTLRInputStream input(fileText);
 	NowLexer lexer(&input);
 	CommonTokenStream tokens(&lexer);
 
